efa9473c157c54fad4927da43d3ba0918d0ab487
Angsuman Barua <angsumanbarua11@gmail.com>
2025-02-08 23:54:12 +0600

    final update

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Namer App',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyAppController extends GetxController {
+  var current = WordPair.random().obs;
+
+  void newword(){
+    current.value=WordPair.random();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final MyAppController controller = Get.put(MyAppController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('A random word:'),
+            Obx(() => Text(controller.current.value.asLowerCase)),
+            ElevatedButton(
+              onPressed: controller.newword,
+              child: Text("Generate another word"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
